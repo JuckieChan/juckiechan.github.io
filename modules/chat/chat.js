5d6c2003cbcd80a45cb58fe7be9c5a95b627a31c
JuckieChan <luksor144@gmail.com>
2023-12-16 16:18:31 +0400

    chat scrolling feature fix, styles var rework, users class added, files reorganization

--- modules/chat/chat.js
+++ modules/chat/chat.js
@@ -1,8 +1,10 @@
 /* eslint-disable */
 
+import { users } from '../users-alpha/users-alpha.js'
+
 const chat = document.querySelector('#chat');
 
-function createNewMessage (fromWho, currentTime, userPhotoUrl, text) {
+function createNewMessage (userId, currentTime, text) {
     const message = document.createElement('div');
     const messageInfo = document.createElement('div');
     const userPhoto = document.createElement('div');
@@ -18,14 +20,15 @@ function createNewMessage (fromWho, currentTime, userPhotoUrl, text) {
     userPhoto.classList.add("user-photo");
     userInfo.classList.add("chat-user-info");
     userName.classList.add("user-name");
+    userName.setAttribute("data-role", `${userId.role}`)
     sendingTime.classList.add("sending-time");
     bottomBorder.classList.add("bottom-border");
     messageContent.classList.add("message-content");
 
-    userName.textContent = fromWho;
+    userName.textContent = userId.login;
     sendingTime.textContent = "Сегодня, в " + currentTime;
     messageContent.textContent = `${text}`;
-    img.src = fromWho === "JuckieChan" ? "./pictures/juckiechan-logo.svg" : userPhotoUrl;
+    img.src = userId.photoURL;
 
     userPhoto.append(img);
     userInfo.append(userName, sendingTime, bottomBorder)
@@ -38,28 +41,40 @@ function createNewMessage (fromWho, currentTime, userPhotoUrl, text) {
 function getCurrentTime () {
     return String(new Date()).slice(15, 24);
 }
-function addNewMessage (author, text) {
-    chat.append(createNewMessage(author, getCurrentTime(), './pictures/no-avatar.png', text))
-    chat.scrollTo({
-        top: 9999,
-        behavior: "smooth"
-      });
+
+let isChatFullScrolled = true;
+
+chat.addEventListener("scroll", () => {
+    isChatFullScrolled = chat.scrollHeight === chat.scrollTop + 350 ? true : false;
+})    
+
+function addNewMessage (userID, text) {
+    chat.append(createNewMessage(users[userID], getCurrentTime(), text))
+    if (isChatFullScrolled) {
+        chat.scrollTo({
+            top: 9999,
+            behavior: "smooth"
+        });
+    }
 }
+
 
 const addNewMessageWithTimeout = (author, text, timeout) => setTimeout(() => addNewMessage(author, text), timeout)
 
-addNewMessageWithTimeout('Luksor', `А чо там чат починили чишо?`, 1000)
-addNewMessageWithTimeout('pro100gamer2013', `та нет канечно кагда там чат админы пачинят сто лет прайдет`, 10000)
-addNewMessageWithTimeout('Stark-333', `Букварь этому господину`, 16000)
-addNewMessageWithTimeout('pro100gamer2013', `себе купи`, 20000)
-addNewMessageWithTimeout('JuckieChan', `К сожалению, с чатом возникли некоторые трудности, просьба отнестись к этому с пониманием. Это касается только новых пользователей, вы можете общаться как и раньше.`, 23000)
-addNewMessageWithTimeout('pro100gamer2013', `я же гаварил`, 29000)
-addNewMessageWithTimeout('JuckieChan', `Терпение, друзья. Как только так сразу.`, 36000)
-addNewMessageWithTimeout('Stark-333', `Макс, там это, пиши если помощь нужна, поможем чем сможем`, 39000)
-addNewMessageWithTimeout('JuckieChan', `От души бро`, 42000)
-addNewMessageWithTimeout('pro100gamer2013', `лан я пашол гамать чао какао`, 49000)
-addNewMessageWithTimeout('Luksor', `И что это было?...`, 55000)
-addNewMessageWithTimeout('Stark-333', `Хз))`, 58000)
+addNewMessageWithTimeout('id_000001', `Тест`, 1000)
+addNewMessageWithTimeout('id_000001', `Тест`, 1000)
+addNewMessageWithTimeout('id_000001', `Тест`, 1000)
+addNewMessageWithTimeout('id_000001', `Тест`, 1000)
 
-// сделать класс User с пользователями и тд, использовать его при отправке сообщений
-// сделать объект с группами пользователей с парой ключ-название и значение-цвет
+addNewMessageWithTimeout('id_000002', `А чо там чат починили чишо?`, 1000)
+addNewMessageWithTimeout('id_000003', `та нет канечно кагда там чат админы пачинят сто лет прайдет`, 10000)
+addNewMessageWithTimeout('id_000004', `Букварь этому господину`, 16000)
+addNewMessageWithTimeout('id_000003', `себе купи`, 20000)
+addNewMessageWithTimeout('id_000001', `К сожалению, с чатом возникли некоторые трудности, просьба отнестись к этому с пониманием. Это касается только новых пользователей, вы можете общаться как и раньше.`, 23000)
+addNewMessageWithTimeout('id_000003', `я же гаварил`, 29000)
+addNewMessageWithTimeout('id_000001', `Терпение, друзья. Как только так сразу.`, 36000)
+addNewMessageWithTimeout('id_000004', `Макс, там это, пиши если помощь нужна, поможем чем сможем`, 39000)
+addNewMessageWithTimeout('id_000001', `От души бро`, 42000)
+addNewMessageWithTimeout('id_000003', `лан я пашол гамать чао какао`, 49000)
+addNewMessageWithTimeout('id_000002', `И что это было?...`, 55000)
+addNewMessageWithTimeout('id_000004', `Хз))`, 58000)
---
+++ modules/users-alpha/users-alpha.js
@@ -0,0 +1,23 @@
+class User {
+    constructor (login, firstname, lastname, email, password, gender, role, photoURL) {
+        this.login = login;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.role = role;
+        this.photoURL = photoURL || `/modules/users-alpha/user-photos/no-avatar.png`;
+    }
+}
+
+const users = {
+    id_000001: new User ("JuckieChan", "Макс", "Анохин", "juckiechan@mail.ru", "2017", "attack helikopter", "admin", "/modules/users-alpha/user-photos/id_000001.svg"),
+    id_000002: new User ("Luksor144", "Мистер", "Фриман", "freeman@hl.com", "iloveheadcrabs", "male", "moderator", "/modules/users-alpha/user-photos/id_000002.png"),
+    id_000003: new User ("Pro100Max2015", "Вася", "Пупкин", "vasya.pupkin2015@mail.ru", "ilovegames", "male", "user", "/modules/users-alpha/user-photos/id_000003.jpg"),
+    id_000004: new User ("Stark-333", "Эцио", "Аудиторе", "ecio@assasin.com", "ihatetampliers", "attack helikopter", "vip", "/modules/users-alpha/user-photos/id_000004.png"),
+    id_000005: new User ("Mithrandir", "Гендальф", "Серый", "gendalf-gray@gmail.com", "ilovehobbits", "male", "vip", ""),
+    id_000005: new User ("Пещерный тролль", "ыаыаы", "уауауаау", "euaaueeueau@gmail.com", "ueueuauau", "male", "troll", ""),
+}
+
+export {users}
