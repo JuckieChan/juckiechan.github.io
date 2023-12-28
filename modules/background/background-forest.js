cc9e37cad73abdd55702c6b52b74ccd56d0438a7
JuckieChan <luksor144@gmail.com>
2023-12-28 22:13:18 +0400

    background initial

---
+++ modules/background/background-forest.js
@@ -0,0 +1,7 @@
+function createBackgroundForest () {
+    const forest = document.createElement("div");
+    forest.className = "background-forest"
+    document.body.prepend(forest)
+}
+
+createBackgroundForest();
